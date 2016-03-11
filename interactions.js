0b985e53cbc09bb69e2e83f2cc69e029bfcdd2db
Tim Follo <timothy.follo@yale.edu>
2016-03-11 12:37:43 -0500

    got test dragging working

--- interactions.js
+++ interactions.js
@@ -1,24 +1,43 @@
+var no_handle = true;
+
 // target elements with the "draggable" class
-interact('.draggable')
-  .draggable({
+var pic = interact('.draggable');
+
+pic.draggable({
+    max: 5,
+    manualStart: false,                      // will not work b/c you need to make a new listener [use no_handle]
     // enable inertial throwing
     inertia: false,
     // keep the element within the area of it's parent
     restrict: {
       restriction: "parent",
       endOnly: true,
-      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+      elementRect: { top: 0.75, left: 0.75, bottom: -2, right: 0.25 }
     },
-    // enable autoScroll
-    autoScroll: false,
 
+    onstart: function (event) {},// fade in?
     // call this function on every dragmove event
     onmove: dragMoveListener,
     // call this function on every dragend event
-    onend: function (event) {}
+    onend: function (event) {} // fade out?
   });
 
+// interact('.draggable').on("down", function (event) {
+//   console.log("triggerred");
+// });
+
   function dragMoveListener (event) {
+    console.log("drag");
+
+    // var x = event.pageX - $('#element').offset().left;
+    // var y = event.pageY - $('#element').offset().top;
+
+    // var x_offset = event.pageX - $('#face4').offset().left;
+    // var y_offset = event.pageY - $('#face4').offset().top;
+
+    // console.log(x_offset);
+    // console.log(y_offset);
+
     var target = event.target,
         // keep the dragged position in the data-x/data-y attributes
         x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
@@ -33,3 +52,70 @@ interact('.draggable')
     target.setAttribute('data-x', x);
     target.setAttribute('data-y', y);
   }
+
+  function checkBoundsFace4 (event){
+    var x_offset = event.pageX - $('#face4').offset().left;
+    var y_offset = event.pageY - $('#face4').offset().top;
+
+    // console.log(x_offset);
+    // console.log(y_offset);
+
+    if (x_offset > 500) {
+      console.log(x_offset);
+      pic.draggable(false);   
+    } else {
+      pic.draggable(true);
+    };
+  }
+
+  pic.on("down", checkBoundsFace4);
+
+// interact(".drag-trigger").on("down", function (event) {
+//     console.log("triggered");
+//     var interaction = event.interaction;
+
+//     // interact('.draggable').fire("down");
+//     // interaction.fire();
+//     interaction.start({ name: 'drag' },
+//                         event.interactible,
+//                         $('.draggable'))
+// });
+
+
+
+function myHover(){
+  console.log("trigger");
+  no_handle = false;
+}
+
+function myLeave(){
+  console.log("leave");
+  no_handle = true;
+}
+
+function mmd(){
+console.log("down");
+
+// $('.draggable').trigger("drag");
+}
+
+// function mmv(event){
+//   console.log("move");
+
+//   var interaction = event.interaction;
+
+//   if (interaction.pointerIsDown) {
+//     dragMoveListener(event);
+//   };
+// }
+
+function mmu(){
+  console.log("up");
+}
+
+$('map').imageMapResize();
+
+// interact('body').preventDefault("always");
+
+$('map').css("pointer-events","none");
+$('area').css("pointer-events","none");
