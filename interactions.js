49362a43137bc08c2bbce5f99627447f2a795d32
Tim Follo <timothy.follo@yale.edu>
2016-03-12 11:49:51 -0500

    specified face4 -- TODO: refactor repetitive code

--- interactions.js
+++ interactions.js
@@ -1,9 +1,26 @@
-var no_handle = true;
-
 // target elements with the "draggable" class
-var pic = interact('.draggable');
+// var pic = interact('.draggable');
 
-pic.draggable({
+// pic.draggable({
+//     max: 5,
+//     manualStart: false,                      // will not work b/c you need to make a new listener [use no_handle]
+//     // enable inertial throwing
+//     inertia: false,
+//     // keep the element within the area of it's parent
+//     restrict: {
+//       restriction: "parent",
+//       endOnly: true,
+//       elementRect: { top: 0.75, left: 0.75, bottom: -2, right: 0.25 }
+//     },
+
+//     onstart: function (event) {},// fade in?
+//     // call this function on every dragmove event
+//     onmove: dragMoveListener,
+//     // call this function on every dragend event
+//     onend: function (event) {} // fade out?
+//   });
+
+var face1_object = interact('#face1').draggable({
     max: 5,
     manualStart: false,                      // will not work b/c you need to make a new listener [use no_handle]
     // enable inertial throwing
@@ -21,6 +38,79 @@ pic.draggable({
     // call this function on every dragend event
     onend: function (event) {} // fade out?
   });
+var face2_object = interact('#face2').draggable({
+    max: 5,
+    manualStart: false,                      // will not work b/c you need to make a new listener [use no_handle]
+    // enable inertial throwing
+    inertia: false,
+    // keep the element within the area of it's parent
+    restrict: {
+      restriction: "parent",
+      endOnly: true,
+      elementRect: { top: 0.75, left: 0.75, bottom: -2, right: 0.25 }
+    },
+
+    onstart: function (event) {},// fade in?
+    // call this function on every dragmove event
+    onmove: dragMoveListener,
+    // call this function on every dragend event
+    onend: function (event) {} // fade out?
+  });
+var face3_object = interact('#face3').draggable({
+    max: 5,
+    manualStart: false,                      // will not work b/c you need to make a new listener [use no_handle]
+    // enable inertial throwing
+    inertia: false,
+    // keep the element within the area of it's parent
+    restrict: {
+      restriction: "parent",
+      endOnly: true,
+      elementRect: { top: 0.75, left: 0.75, bottom: -2, right: 0.25 }
+    },
+
+    onstart: function (event) {},// fade in?
+    // call this function on every dragmove event
+    onmove: dragMoveListener,
+    // call this function on every dragend event
+    onend: function (event) {} // fade out?
+  });
+var face4_object = interact('#face4').draggable({
+    max: 5,
+    manualStart: false,                      // will not work b/c you need to make a new listener [use no_handle]
+    // enable inertial throwing
+    inertia: false,
+    // keep the element within the area of it's parent
+    restrict: {
+      restriction: "parent",
+      endOnly: true,
+      elementRect: { top: 0.75, left: 0.75, bottom: -2, right: 0.25 }
+    },
+
+    onstart: function (event) {},// fade in?
+    // call this function on every dragmove event
+    onmove: dragMoveListener,
+    // call this function on every dragend event
+    onend: function (event) {} // fade out?
+  });
+var face5_object = interact('#face5').draggable({
+    max: 5,
+    manualStart: false,                      // will not work b/c you need to make a new listener [use no_handle]
+    // enable inertial throwing
+    inertia: false,
+    // keep the element within the area of it's parent
+    restrict: {
+      restriction: "parent",
+      endOnly: true,
+      elementRect: { top: 0.75, left: 0.75, bottom: -2, right: 0.25 }
+    },
+
+    onstart: function (event) {},// fade in?
+    // call this function on every dragmove event
+    onmove: dragMoveListener,
+    // call this function on every dragend event
+    onend: function (event) {} // fade out?
+  });
+
 
 // interact('.draggable').on("down", function (event) {
 //   console.log("triggerred");
@@ -57,73 +147,17 @@ pic.draggable({
     var x_offset = event.pageX - $('#face4').offset().left;
     var y_offset = event.pageY - $('#face4').offset().top;
 
-    // console.log(x_offset);
-    // console.log(y_offset);
-
-      // console.log(x_offset);
-      // pic.draggable(false);  
-
     if (x_offset < 24) {
-      if (y_offset > (166.0 * x_offset)/24.0) {pic.draggable(false);} else {pic.draggable(true);};
+      if (y_offset > (166.0 * x_offset)/24.0) {face4_object.draggable(false);} else {face4_object.draggable(true);};
     } else if (x_offset < 518) {
-      if (y_offset > ((-112.0 * x_offset)/506.0 + 171.3)) {pic.draggable(false);} else {pic.draggable(true);};
+      if (y_offset > ((-112.0 * x_offset)/506.0 + 171.3)) {face4_object.draggable(false);} else {face4_object.draggable(true);};
     } else if (x_offset < 530) {
       if (y_offset < 54) {
-        if (y_offset < ((54.0 * x_offset)/12.0 - 2331)) {pic.draggable(false);} else {pic.draggable(true);};
+        if (y_offset < ((54.0 * x_offset)/12.0 - 2331)) {face4_object.draggable(false);} else {face4_object.draggable(true);};
       } else {
-        if (y_offset > ((112.0 * x_offset)/506.0 + 171.3)) {pic.draggable(false);} else {pic.draggable(true);};
+        if (y_offset > ((112.0 * x_offset)/506.0 + 171.3)) {face4_object.draggable(false);} else {face4_object.draggable(true);};
       };
-    } else {pic.draggable(true);};
+    } else {face4_object.draggable(true);};
   }
 
-  pic.on("down", checkBoundsFace4);
-
-// interact(".drag-trigger").on("down", function (event) {
-//     console.log("triggered");
-//     var interaction = event.interaction;
-
-//     // interact('.draggable').fire("down");
-//     // interaction.fire();
-//     interaction.start({ name: 'drag' },
-//                         event.interactible,
-//                         $('.draggable'))
-// });
-
-
-
-function myHover(){
-  console.log("trigger");
-  no_handle = false;
-}
-
-function myLeave(){
-  console.log("leave");
-  no_handle = true;
-}
-
-function mmd(){
-console.log("down");
-
-// $('.draggable').trigger("drag");
-}
-
-// function mmv(event){
-//   console.log("move");
-
-//   var interaction = event.interaction;
-
-//   if (interaction.pointerIsDown) {
-//     dragMoveListener(event);
-//   };
-// }
-
-function mmu(){
-  console.log("up");
-}
-
-$('map').imageMapResize();
-
-// interact('body').preventDefault("always");
-
-$('map').css("pointer-events","none");
-$('area').css("pointer-events","none");
+  face4_object.on("down", checkBoundsFace4);
